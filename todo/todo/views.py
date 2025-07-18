6c5469602fe5e50ed211a0b0377b444e8e804233
Sasank5716 <sasanklama5716@gmail.com>
2025-07-18 08:35:12 +0545

    Store inputs from signupform and redirect to login page

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import TodoItem
 
 def signup(request):
+    if request.method == 'POST':
+        fnm = request.POST.get('fnm')
+        emailid = request.POST.get('emailid')
+        pwd = request.POST.get('pwd')
+        print(fnm, emailid, pwd)
+        myuser = User.objects.create_user(username=fnm, email=emailid, password=pwd)
+        myuser.save()
+        return redirect('/login')
     return render(request, 'signup.html')
 
 def login(request):
