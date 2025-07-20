d07c4ca6d8e2baca5533a85f891903ed315edef6
Sasank5716 <sasanklama5716@gmail.com>
2025-07-20 17:10:13 +0545

    Connect database

---
+++ crm_app/dcrm/mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="sasank07",
+    database="sasank"
+)
+
+# prepare a cursor object using the cursor() method
+cursorObject = database.cursor()
+
+#create database
+cursorObject.execute("CREATE DATABASE sasank")
+
+print("Database created successfully")
