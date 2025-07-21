10fc05ae212e9f92df839a6f88f79d032e17fc8b
Sasank5716 <sasanklama5716@gmail.com>
2025-07-21 15:37:55 +0545

    Add login view

--- crm_app/dcrm/website/views.py
+++ crm_app/dcrm/website/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html',{})
 
 def login_user(request):
-    if request.Method == 'POST':
-        
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Log the user in
+            login(request, user)
+            messages.success(request, 'Login successful')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
     return render(request, 'login.html', {}) 
 
 def logout_user(request):
