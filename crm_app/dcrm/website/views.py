b5ee0cf424d4269a200deba67ba20129da3b076c
Sasank5716 <sasanklama5716@gmail.com>
2025-07-21 15:16:05 +0545

    create a login and logout users

--- crm_app/dcrm/website/views.py
+++ crm_app/dcrm/website/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html',{})
+
+def login_user(request):
+    if request.Method == 'POST':
+        
+    return render(request, 'login.html', {}) 
+
+def logout_user(request):
+    pass
+
+
